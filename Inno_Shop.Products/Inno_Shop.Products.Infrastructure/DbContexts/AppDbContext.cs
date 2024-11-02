f614e8a734ecea096c97714d8ab829298640264b
westcrime <westcrime77@rambler.ru>
2024-11-02 23:28:41 +0300

    working

--- Inno_Shop.Products/Inno_Shop.Products.Infrastructure/DbContexts/AppDbContext.cs
+++ Inno_Shop.Products/Inno_Shop.Products.Infrastructure/DbContexts/AppDbContext.cs
@@ -10,12 +10,5 @@ namespace Inno_Shop.Products.Infrastructure.DbContexts
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-
-        }
     }
 }
--- Inno_Shop.Users/Inno_Shop.Users.Infrastructure/DbContext/AppDbContext.cs
+++ Inno_Shop.Users/Inno_Shop.Users.Infrastructure/DbContext/AppDbContext.cs
@@ -8,11 +8,4 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-
-    }
 }
